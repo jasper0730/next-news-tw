a88d7bb37c0c4c26a5ca39e611403df00a9c5df0
Jasper Li <wilsonlee0730@gmail.com>
2023-11-06 15:43:25 +0000

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+
+import React from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import { Header, Footer } from '@/components/layout'
+import styled from 'styled-components';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
---
+++ components/layout/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+	return (
+		<>
+			<nav>
+				footer
+			</nav>
+		</>
+	)
+}
+
+export default Footer
